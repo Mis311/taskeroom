23c27ba11f8ed15f22b1b25cba9adb8385b7a8d4
Giacomo Pessotto <giacomopesso@gmail.com>
2023-05-19 17:52:15 +0100

    added firebase auth functions and initialization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Layout from '@/components/layouts/layout';
 import Home from '.';
 import '../styles/globals.css'
+import { AuthProvider } from '../firebase/AuthContext';
 
 const App: React.FC = () => {
     return (
+        <AuthProvider>
         <Layout>
             <Home />
         </Layout>
+        </AuthProvider>
     )
 }
 
