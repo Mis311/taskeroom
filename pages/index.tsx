942f4a44ffe4d8c49397212f182dcb9ff874a354
Mis311 <ley.hanamura@gmail.com>
2023-06-03 16:55:26 +0200

    to tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,53 @@
 import React from "react";
 import Layout from "@/components/layouts/layout";
-// import MyCalendarComponent from '../components/MyCalendar';
-import styles from "./index.module.css";
 
 const Home: React.FC = () => {
   return (
     <Layout noLayout={true}>
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <h1>Taskeroom</h1>
-          <button className={styles.loginBtn}>Login</button>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <header className="flex items-center justify-between w-full px-4 py-2">
+          <h1 className="text-2xl font-bold text-white">Taskeroom</h1>
+          <button className="px-4 py-2 text-white rounded-lg bg-pink-400 hover:bg-pink-500 transition-colors duration-300">Login</button>
         </header>
 
-        <div className={styles.section}>
-          <section>
-            <h2 className={styles.tagline}>
+        <div className="flex flex-col md:flex-row md:space-x-10 space-y-10 md:space-y-0">
+          <section className="flex flex-col items-center text-center">
+            <h2 className="text-3xl font-bold">
               Automate and go ahead of your life
             </h2>
 
-            <p className={styles.subtitle}>
+            <p className="mt-4 text-xl text-gray-500">
               Let's get things done and complete your life together with AI and
               automate execution sessions with your new friends.
             </p>
 
-            <div className={styles.inputSection}>
+            <div className="flex items-center justify-center mt-8 space-y-4 flex-col md:flex-row md:space-y-0 md:space-x-4">
               <input
-                className={styles.inputGoal}
+                className="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:border-pink-400 text-center md:w-2/3"
                 placeholder="Input Your Goal"
               />
-              <button className={styles.automateButton}>Automate ➡</button>
+              <button className="w-full px-4 py-2 text-white rounded-lg bg-pink-400 hover:bg-pink-500 transition-colors duration-300 md:w-auto">Automate ➡</button>
             </div>
           </section>
 
-          <section>
-            <div className={styles.imgContainer}>
-              <img src="calendar.png" alt="calendar" />
+          <section className="flex flex-col items-center justify-center">
+            <div className="w-64 h-64">
+              <img className="object-cover w-full h-full" src="calendar.png" alt="calendar" />
             </div>
-            {/* <MyCalendarComponent sessions={[]} /> */}
           </section>
 
-          <section>
-            <h2 style={{ textAlign: "right" }}>
-              May <span style={{ fontSize: "2em" }}>23rd</span> 2023
+          <section className="flex flex-col items-center">
+            <h2 className="text-3xl text-right">
+              May <span className="text-6xl">23rd</span> 2023
             </h2>
 
-            <ul className={styles.todoList}>
-              <li>todo1</li>
-              <li>todo2</li>
-              <li>todo3</li>
+            <ul className="flex flex-col space-y-2">
+              <li className="text-2xl border-b border-gray-400">todo1</li>
+              <li className="text-2xl border-b border-gray-400">todo2</li>
+              <li className="text-2xl border-b border-gray-400">todo3</li>
             </ul>
 
-            <button className={styles.arrowButton}>➡</button>
+            <button className="w-16 h-16 mt-12 text-white bg-black rounded-full cursor-pointer">➡</button>
             <p>Automate</p>
           </section>
         </div>
--- pages/taskMaker.tsx
+++ pages/taskMaker.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { config } from "dotenv";
 config();
-import styles from "./taskMaker.module.css";
 
 const TaskMaker = () => {
   const [taskDescription, setTaskDescription] = useState("");
@@ -43,24 +42,25 @@ const TaskMaker = () => {
     // Add code to add the task to your dashboard or whatever else you need to do with the task
   };
   
-  return (
-    <div className={styles.container}>
-      <h2>Add Your Task</h2>
-      <form className={styles.form} onSubmit={handleTaskSubmit}>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="What is your goal/todo?"
-          value={taskDescription}
-          onChange={(e) => setTaskDescription(e.target.value)}
-        />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="When is the deadline?e.g. 1month, July 3rd 2023..."
-          value={deadline}
-          onChange={(e) => setDeadline(e.target.value)}
-        />
+
+    return (
+      <div className="flex flex-col items-center p-4 font-sans text-gray-800 bg-gray-200 w-full min-w-full">
+        <h2>Add Your Task</h2>
+        <form className="flex flex-col mb-8 w-3/5" onSubmit={handleTaskSubmit}>
+          <input
+            className="w-full mb-4 p-2 border-none rounded-lg shadow-md"
+            type="text"
+            placeholder="What is your goal/todo?"
+            value={taskDescription}
+            onChange={(e) => setTaskDescription(e.target.value)}
+          />
+          <input
+            className="w-full mb-4 p-2 border-none rounded-lg shadow-md"
+            type="text"
+            placeholder="When is the deadline?e.g. 1month, July 3rd 2023..."
+            value={deadline}
+            onChange={(e) => setDeadline(e.target.value)}
+          />
         <label>
           <input
             type="checkbox"
@@ -85,20 +85,21 @@ const TaskMaker = () => {
           />
           AI Suggest what AI can do
         </label>
-        <button type="submit">Confirm Task</button>
-      </form>
-
-      <h2>AI Suggestions</h2>
-      <div className={styles.suggestions}>
-        {aiSuggestions &&
-          aiSuggestions.map((suggestion, index) => (
-            <div key={index}>
-              <p>{suggestion}</p>
-            </div>
-          ))}
+          <button className="p-2 border-none rounded-lg text-white bg-purple-500 cursor-pointer transition-colors duration-300 hover:bg-purple-600 w-1/5" type="submit">Confirm Task</button>
+        </form>
+  
+        <h2>AI Suggestions</h2>
+        <div className="flex flex-col items-center">
+          {aiSuggestions &&
+            aiSuggestions.map((suggestion, index) => (
+              <div key={index} className="w-full p-4 mb-4 text-center text-gray-800 bg-white rounded-lg shadow-md">
+                <p>{suggestion}</p>
+              </div>
+            ))}
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default TaskMaker;
+    );
+  };
+  
+  export default TaskMaker;
+  
