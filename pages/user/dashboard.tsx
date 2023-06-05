21b96b5e79c4ba0fa63f695aa31befea6bb3ba4f
Mis311 <ley.hanamura@gmail.com>
2023-06-05 16:47:48 +0200

    dashboard overall progress bar added

--- pages/user/dashboard.tsx
+++ pages/user/dashboard.tsx
@@ -4,46 +4,53 @@ import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import Image from "next/image";
 const Dashboard = () => {
-  const todos = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6"];
+  const todos = [
+    "Inventory System Optimization",
+    "Campaign Planning",
+    "Social Media Update",
+    "Product Description",
+    "CS Training",
+    "Customer Feedback",
+  ];
 
   const localizer = momentLocalizer(moment);
   const events: any[] = []; // to add events
 
   return (
-    <div className="flex h-screen ml-56">
+    <div className="flex flex-col h-screen ml-56 ">
+      {/* Overall Progress */}
+      <div className="w-full p-4 text-center items-center flex justify-center border-b border-white">
+        <Image
+          src="/component2.png"
+          alt="Overall Progress"
+          width={800}
+          height={200}
+        />
+      </div>
+      <div className="w-full bg-white h-3"></div>
       <div className="flex flex-grow">
         <div className="w-1/4 p-4 ">
           <h1 className="text-2xl font-bold text-purple-700 mb-4">
-            Seller&apos;s Todo Lists Todo List
+            Seller&apos;s Todo Lists
           </h1>
           {todos.map((todo, index) => (
             <div
               key={index}
-              className="bg-purple-200 rounded-lg p-4 mb-4 flex justify-between items-center"
+              className="bg-purple-200 rounded-lg p-2 mb-4 flex justify-between items-center cursor-pointer"
             >
-              <p className="text-purple-700">{todo}</p>
-              <div>
-                <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-1 px-2 rounded mr-2">
-                  Edit
-                </button>
-                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
-                  Remove
-                </button>
-              </div>
+              <p className="text-purple-600">{todo}</p>
             </div>
           ))}
         </div>
 
         {/* Progress tree */}
         <div className="w-2/4 p-4 ">
-        
           <Image
             src="/component.png"
             alt="barchart"
             width={500}
             height={800}
           ></Image>
-          {/* to be replaced */}
         </div>
         {/* Schedule booking */}
         <div className="w-1/4  p-4">
