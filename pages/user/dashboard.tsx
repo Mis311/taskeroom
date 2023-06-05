fdbd9bc1a482a50e34ba275fe6d7661dc882553c
Mis311 <ley.hanamura@gmail.com>
2023-06-05 15:46:52 +0200

    update chart on user dashboard

--- pages/user/dashboard.tsx
+++ pages/user/dashboard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-
+import Image from "next/image";
 const Dashboard = () => {
   const todos = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6"];
 
@@ -12,7 +12,7 @@ const Dashboard = () => {
   return (
     <div className="flex h-screen ml-56">
       <div className="flex flex-grow">
-        <div className="w-1/4 p-4 overflow-y-scroll">
+        <div className="w-1/4 p-4 ">
           <h1 className="text-2xl font-bold text-purple-700 mb-4">
             Seller&apos;s Todo Lists Todo List
           </h1>
@@ -35,11 +35,13 @@ const Dashboard = () => {
         </div>
 
         {/* Progress tree */}
-        <div className="w-2/4 bg-gray-200 p-4 overflow-y-scroll">
+        <div className="w-2/4 p-4 ">
           <p>Progress tree</p>
+          <Image src="/barchart.png" alt="progress-tree" width={300} height={400} ></Image>
+          {/* to be replaced */}
         </div>
         {/* Schedule booking */}
-        <div className="w-1/4 bg-gray-200 p-4 overflow-y-scroll">
+        <div className="w-1/4  p-4">
           <Calendar
             localizer={localizer}
             events={events}
